ea9e20c427adde45f93627dcf1c5fd046cc594c9
arkosh <trukan24@gmail.com>
2023-06-01 08:28:22 +0300

    :recycle: fix imports

--- internal/api/access_v1/check.go
+++ internal/api/access_v1/check.go
@@ -3,15 +3,16 @@ package access_v1
 import (
 	"context"
 
-	"github.com/golang/protobuf/ptypes/empty"
-
 	desc "github.com/Arkosh744/auth-service-api/pkg/access_v1"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (i *Implementation) CheckAccess(ctx context.Context, req *desc.CheckAccessRequest) (*empty.Empty, error) {
 	err := i.accessService.CheckAccess(ctx, req.GetEndpoint())
 	if err != nil {
-		return &empty.Empty{}, err
+		return &empty.Empty{}, status.Error(codes.PermissionDenied, "access denied")
 	}
 
 	return &empty.Empty{}, nil
